cf979c3320bc8abb4544ef013aae172e2962428e
Dushyant Juneja <juneja.dushyant@gmail.com>
2022-09-27 16:06:28 -0400

    Refactor to remove obsolete code

--- main.go
+++ main.go
@@ -188,33 +188,6 @@ func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func showHelp(w http.ResponseWriter, r *http.Request) {
-	help := `
-	Hello there! :)
-	This is a simple demo API created using Golang. It implements a trivial customer relationship management backend.
-	Each customer has an entry as follows:
-
-	Id        uint
-	Name      string
-	Role      string
-	Email     string
-	Phone     string
-	Contacted bool
-
-	They are stored serially in an in-memory database. Which is more or less a glorified map.
-
-	The API runs on port 8000. The following requests are available:
-	- Getting a single customer through a http://localhost:8000/customers/{id} path
-	- Getting all customers through a the http://localhost:8000/customers path
-	- Creating a customer through a http://localhost:8000/customers path
-	- Updating a customer through a http://localhost:8000/customers/{id} path
-	- Deleting a customer through a http://localhost:8000/customers/{id} path
-
-	Enjoy!
-	`
-	fmt.Fprintf(w, help)
-}
-
 func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/index.html")
 }
@@ -230,7 +203,6 @@ func main() {
 		- Deleting a customer through a /customers/{id} path
 	*/
 	router := mux.NewRouter()
-	router.HandleFunc("/", index).Methods("GET")
 	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
 	router.HandleFunc("/customers", getCustomers).Methods("GET")
 	router.HandleFunc("/customers", addCustomer).Methods("POST")
@@ -238,7 +210,7 @@ func main() {
 	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
 
 	// A home route to briefly describe the API
-	router.HandleFunc("/", showHelp).Methods("GET")
+	router.HandleFunc("/", index).Methods("GET")
 
 	// Make it accessible at localhost:8000
 	fmt.Println("Started at http://localhost:8000!")
